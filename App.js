68a8b60e572341b152425779c7b04bab2643ddef
laine <laine@kaigen.us>
2020-10-11 11:20:58 -0700

    display chapter links on new screen

--- App.js
+++ App.js
@@ -1,9 +1,50 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button, } from 'react-native';
+import { View, Text, TextInput, Button, ScrollView } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-import { homeScreen, } from './components.js';
+// import { homeScreen, chapterList, } from './components.js';
+import { getFictionPage, } from './network.js';
+import { parseForChapterLinks, } from './parse.js';
+
+const fictionPageButton = async (fictionId, { navigation }) => {
+  var fictionPage =
+    await getFictionPage(fictionId)
+    .then(response => {return response;});
+  var chapterLinks = parseForChapterLinks(fictionPage);
+  navigation.navigate('Chapter List', chapterLinks);
+}
+
+export const homeScreen = ({ navigation }) => {
+  const [fictionId, setFictionId] = useState('');
+
+  return (
+    <View style={{padding: 10}}>
+      <TextInput // TODO: Check this raw input
+        style={{height: 40}}
+        placeholder="Fiction Id"
+        onChangeText={text => setFictionId(text)}
+        defaultValue={fictionId}
+      />
+      <Button
+        title="Get Page"
+        onPress={() => fictionPageButton(fictionId, {navigation})}
+      />
+    </View>
+  );
+}
+
+export const chapterList = ({ route, navigation }) => {
+  var chapterLinks = route.params;
+  console.log(chapterLinks);
+  return (
+    <ScrollView>
+      {chapterLinks.map((link) => (
+        <Text>{link}</Text>
+      ))}
+    </ScrollView>
+  );
+}
 
 const Stack = createStackNavigator();
 
@@ -13,6 +54,7 @@ const App = () => {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={homeScreen} />
+        <Stack.Screen name="Chapter List" component={chapterList} />
       </Stack.Navigator>
     </NavigationContainer>
   );
