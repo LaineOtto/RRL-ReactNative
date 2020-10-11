5101345815a3037366d34fc23a8ae07252db7347
laine <laine@kaigen.us>
2020-10-11 04:30:36 -0700

    change name of parse.js to network.js to increase accuracy

--- network.js
+++ network.js
@@ -5,16 +5,10 @@ export const getFictionUrl = async (fictionIdUrl) => {
     await fetch(fictionIdUrl) // TODO: put in separate file so I can return the url as a string
     .then(response => response.url)
     .then(url => {
-      console.log("thenurl: " + url);
       return url;
     })
     .catch((error) => {
       console.error(error);
-    });
+  });
   return fictionUrl;
-};
-
-export const fetchUrlAsync = async (fictionIdUrl) => {
-  var fictionUrl = getFictionUrl(fictionIdUrl);
-  return await fictionUrl;
 };
