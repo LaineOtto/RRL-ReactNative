26b03250cd8f25595d98db6191e58351c0b05967
laine <laine@kaigen.us>
2020-10-13 04:47:47 -0700

    seperate components from main file

--- App.js
+++ App.js
@@ -1,86 +1,8 @@
-import React, { useState, Suspense, useEffect } from 'react';
-import { View, Text, TextInput, Button, ScrollView, useWindowDimensions } from 'react-native';
+import React, { useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { WebView } from 'react-native-webview';
-import HTML from "react-native-render-html";
 
-import { getFictionPage, getPageText, } from './network.js';
-import { parseForChapterLinks, parseChapterContent } from './parse.js';
-
-const fictionPageButton = async (fictionId, { navigation }) => {
-  var fictionPage =
-    await getFictionPage(fictionId)
-    .then(response => {return response;});
-  var chapterLinks = parseForChapterLinks(fictionPage);
-  console.log(chapterLinks);
-  navigation.navigate('Chapter List', chapterLinks);
-}
-
-const homeScreen = ({ navigation }) => {
-  const [fictionId, setFictionId] = useState('');
-
-  return (
-    <View style={{padding: 10}}>
-      <TextInput // TODO: Check this raw input
-        style={{height: 40}}
-        placeholder="Fiction Id"
-        onChangeText={text => setFictionId(text)}
-        defaultValue={fictionId}
-      />
-      <Button
-        title="Get Page"
-        onPress={() => fictionPageButton(fictionId, {navigation})}
-      />
-    </View>
-  );
-}
-
-const chapterList = ({ route, navigation }) => {
-  var chapterLinks = route.params;
-  return (
-    <ScrollView>
-      {chapterLinks.map((link) => (
-        <Button
-          title={link}
-          onPress={() => {
-            navigation.navigate('Read Chapter', link);
-          }}
-        />
-      ))}
-    </ScrollView>
-  );
-}
-
-const readChapter = ({ route, navigation }) => {
-  const [chapterUrl, setChapterUrl] = useState(route.params);
-  const [htmlString, setHtmlString] = useState('');
-  console.log("chapterUrl: " + chapterUrl);
-
-  useEffect(() => {
-    const getPageAsync = async () => {
-      var htmlString =
-        await getPageText(chapterUrl)
-        .then(response => {return response;});
-      // console.log("async: " + htmlString);
-
-      setHtmlString(htmlString);
-    };
-
-    getPageAsync(chapterUrl);
-  }, []);
-
-  // console.log("sync: " + htmlString);
-  const chapterContent = parseChapterContent(htmlString);
-  console.log("chapterContent: " + chapterContent);
-
-  const contentWidth = useWindowDimensions().width;
-  return (
-    <ScrollView style={{ flex: 1 }}>
-      <HTML html={chapterContent} contentWidth={contentWidth} />
-    </ScrollView>
-  );
-}
+import { homeScreen, chapterList, readChapter } from './components.js';
 
 const Stack = createStackNavigator();
 
---
+++ components.js
@@ -0,0 +1,85 @@
+/**
+ *  components.js - React rendering components
+ *
+ */
+ import React, { useState, useEffect } from 'react';
+ import { View, Text, TextInput, Button, ScrollView, useWindowDimensions } from 'react-native';
+ import { WebView } from 'react-native-webview';
+ import HTML from "react-native-render-html";
+
+ import { getFictionPage, getPageText, } from './network.js';
+ import { parseForChapterLinks, parseChapterContent } from './parse.js';
+
+ const fictionPageButton = async (fictionId, { navigation }) => {
+   var fictionPage =
+     await getFictionPage(fictionId)
+     .then(response => {return response;});
+   var chapterLinks = parseForChapterLinks(fictionPage);
+   console.log(chapterLinks);
+   navigation.navigate('Chapter List', chapterLinks);
+ };
+
+ export const homeScreen = ({ navigation }) => {
+   const [fictionId, setFictionId] = useState('');
+
+   return (
+     <View style={{padding: 10}}>
+       <TextInput // TODO: Check this raw input
+         style={{height: 40}}
+         placeholder="Fiction Id"
+         onChangeText={text => setFictionId(text)}
+         defaultValue={fictionId}
+       />
+       <Button
+         title="Get Page"
+         onPress={() => fictionPageButton(fictionId, {navigation})}
+       />
+     </View>
+   );
+ };
+
+ export const chapterList = ({ route, navigation }) => {
+   const chapterLinks = route.params;
+   return (
+     <ScrollView>
+       {chapterLinks.map((link) => (
+         <Button
+           title={link}
+           onPress={() => {
+             navigation.navigate('Read Chapter', link);
+           }}
+         />
+       ))}
+     </ScrollView>
+   );
+ };
+
+ export const readChapter = ({ route, navigation }) => {
+   const [chapterUrl, setChapterUrl] = useState(route.params);
+   const [htmlString, setHtmlString] = useState('');
+   console.log("chapterUrl: " + chapterUrl);
+
+   useEffect(() => {
+     const getPageAsync = async () => {
+       var htmlString =
+         await getPageText(chapterUrl)
+         .then(response => {return response;});
+       // console.log("async: " + htmlString);
+
+       setHtmlString(htmlString);
+     };
+
+     getPageAsync(chapterUrl);
+   }, []);
+
+   // console.log("sync: " + htmlString);
+   const chapterContent = parseChapterContent(htmlString);
+   console.log("chapterContent: " + chapterContent);
+
+   const contentWidth = useWindowDimensions().width;
+   return (
+     <ScrollView style={{ flex: 1 }}>
+       <HTML html={chapterContent} contentWidth={contentWidth} />
+     </ScrollView>
+   );
+ };
